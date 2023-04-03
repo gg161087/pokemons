c35e2f9121f0d99df6f22362a11d89d65f6aad7b
gg161087 <gg161087@gmail.com>
2023-04-02 22:51:14 -0300

    subir cambios

--- main.js
+++ main.js
@@ -9,6 +9,58 @@ const fetchUrl = () => {
         for (const iterator of data) {            
             fetch(`${iterator.url}`)
             .then(res => res.json())
+            .then(data => {
+                console.log(data);                
+                if (contador < 151) {  
+                    if(data.id == 1){
+                        carouselInner.innerHTML += `
+                            <div class="carousel-item active">
+                                <div class="card" style="width: 18rem;">                                    
+                                    <img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="${data.name}">
+                                    <div class="card-body">
+                                        <h2>${data.name}</h2>
+                                        <p class="card-text">${data.name}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+                    }else{
+                        carouselInner.innerHTML += `
+                            <div class="carousel-item">
+                                <div class="card" style="width: 18rem;">                                    
+                                    <img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="${data.name}">
+                                    <div class="card-body">
+                                        <p class="card-text">${data.name}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        `                        
+                    }
+                    contador++
+                }              
+            })                     
+        }                    
+    })
+}
+fetchUrl();
+
+
+/* 
+CARD:
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+fetch ANDANDO:
+const fetchUrl = () => {
+    fetch('./database/pokemons.json')
+    .then(res => res.json())
+    .then(data => {        
+        for (const iterator of data) {            
+            fetch(`${iterator.url}`)
+            .then(res => res.json())
             .then(data => {                
                 if (contador < 151) {  
                     if(data.id == 1){
@@ -30,4 +82,4 @@ const fetchUrl = () => {
         }                    
     })
 }
-fetchUrl();
+*/
